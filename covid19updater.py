b898d8306845e443eb7397b3531cb1840394f200
Ibrahim Isiaq Bolaji <96653349+isiaq-ibrahim@users.noreply.github.com>
2025-04-23 17:23:05 +0200

    initial commit

---
+++ covid19updater.py
@@ -0,0 +1,16 @@
+import requests
+from win10toast import ToastNotifier
+import time
+import json
+
+def update():
+    r = requests.get('https://coronavirus-19-api.herokuapp.com/all')
+    data = r.json()
+    text = f'Confirmed Cases : {data["cases"]} \nDeaths : {data["deaths"]} \nRecovered : {data["recovered"]}'
+
+    while True:
+        toast = ToastNotifier()
+        toast.show_toast
+        time.sleep(60)
+
+update()
